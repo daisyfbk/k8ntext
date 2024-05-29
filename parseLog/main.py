637c72f55396f12207506e0b973504c0adead683
varmani <varmani@fbk.eu>
2024-05-29 16:07:05 +0200

    Add python program to parse log

---
+++ parseLog/main.py
@@ -0,0 +1,57 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='parseLog',
+    description='This program takes a log as input and removes from it unnecessary content.'
+                'The output is then written to a new file.')
+
+# Define sets of excluded resources.
+blacklisted_requestURIs = {"/readyz", "/livez", "/api", "/apis", "/openapi/v3?timeout=32s"}
+
+blacklisted_resources_liv2 = {"daemonsets", "deployments", "replicasets", "statefulsets", "cronjobs", "jobs"}
+blacklisted_resources_liv3 = {"persistentvolumeclaims", "persistentvolumes"}
+blacklisted_resources_liv4 = {"endpointslices", "ingressclasses", "ingresses", "services"}
+blacklisted_resources_liv7 = {"leases", "networkpolicies", "priorityclasses", "storageclasses", "csidrivers",
+                              "csinodes", "csistoragecapacities", "volumeattachments", "limitranges", "podtemplates"}
+blacklisted_resources_liv9 = {"mutatingwebhookconfigurations", "validatingwebhookconfigurations", "controllerrevisions",
+                              "runtimeclasses", "events"}
+blacklisted_resources_liv10 = {"customresourcedefinitions", "apiservices", "tokenreviews", "horizontalpodautoscalers",
+                               "flowschemas", "prioritylevelconfigurations", "poddisruptionbudgets"}
+blacklisted_resources_liv20 = {"bindings", "componentstatuses", "endpoints", "replicationcontrollers"}
+
+
+def main():
+    parser.add_argument('-f', required=True, help='The log input file')
+    args = parser.parse_args()
+
+    input_filename = args.f;
+    output_filename = input_filename + "_edited"
+
+    with (open(input_filename, 'r') as input_file):
+        output_file = open(output_filename, "w")
+
+        for line in input_file:
+            # each line is a json, load it
+            json_data = json.loads(line)
+
+            # get the requestURI and filter out the unwanted ones
+            request_uri = json_data['requestURI']
+            if request_uri not in blacklisted_requestURIs:
+
+                # get the resource and filter out the unwanted ones
+                resource = json_data['objectRef']['resource']
+                if resource not in blacklisted_resources_liv2 and \
+                        resource not in blacklisted_resources_liv3 and \
+                        resource not in blacklisted_resources_liv4 and \
+                        resource not in blacklisted_resources_liv7 and \
+                        resource not in blacklisted_resources_liv9 and \
+                        resource not in blacklisted_resources_liv10 and \
+                        resource not in blacklisted_resources_liv20:
+                    print(line, file=output_file, end='')
+
+        output_file.close()
+
+
+if __name__ == "__main__":
+    main()
