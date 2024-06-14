a6b69b797f5331d56a8b767061bf72e9bd8ea4f7
Matteo Franzil <mfranzil@fbk.eu>
2024-06-14 10:20:08 +0200

    Kube-api.sh

--- scripts/kube-api.sh
+++ scripts/kube-api.sh
@@ -7,11 +7,16 @@ if [[ -z "$1" || -z "$2" ]]; then
   exit 1
 fi
 
+server=$1
+path=$2
+shift
+shift
+
 kubectl config view --raw -o json | jq -r '.users[0].user."client-certificate-data"' | base64 -d > client.crt
 kubectl config view --raw -o json | jq -r '.users[0].user."client-key-data"' | base64 -d > client.key
 kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | base64 -d > ca.crt
 
-curl --key client.key --cert client.crt --cacert ca.crt -s "$1/$2"
-
+curl -H "Content-Type: application/json" --key client.key --cert client.crt --cacert ca.crt -s  "$server/$path" "$@" | jq -c
 cd - || exit
 rm -rf "$tmp"
+
