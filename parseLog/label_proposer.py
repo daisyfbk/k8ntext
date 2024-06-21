28638ff71026a1b929b575bc7110e89d63dd2ecf
Matteo Franzil <mfranzil@fbk.eu>
2024-06-21 10:48:49 +0200

    several additions to label_proposer

--- parseLog/label_proposer.py
+++ parseLog/label_proposer.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import functools
+import argparse
 
 LABELS_FILE = 'labels.csv'
 VERBS_FILE = 'verbs.csv'
@@ -32,7 +34,7 @@ labels = load_labels()
 verbs = load_verbs()
 
 
-def generate_label(verb: str, objectRef: dict):
+def generate_label(verb: str, objectRef: dict) -> int:
     """
     We use 20 bits to encode the label
     A First 8 bits: type (at most 255 types, I expect less than 100 types in total)
@@ -65,12 +67,24 @@ def generate_label(verb: str, objectRef: dict):
 
     # print(bin(label[0]), ", ", bin(label[1]), ", ", bin(is_namespaced), ", ", bin(is_single_object), ", ", bin(verb_label))
 
-    label = (label[0] << 8) | (label[1] << 5) | (is_namespaced << 4) | (is_single_object << 3) | verb_label
+    return encode_label(label[0], label[1], is_namespaced, is_single_object, verb_label)
+
+
+@functools.lru_cache(maxsize=None)
+def encode_label(
+    label_id: int,
+    label_sub_id: int,
+    is_namespaced: int,
+    is_single_object: int,
+    verb_id: int
+) -> int:
+    label = (label_id << 8) | (label_sub_id << 5) | (is_namespaced << 4) | (is_single_object << 3) | verb_id
     label = label << 4
 
     return label
 
 
+@functools.lru_cache(maxsize=None)
 def decode_label(label: int):
     if label == -1:
         return "Unknown label"
@@ -99,6 +113,22 @@ def decode_label(label: int):
         "verb": verb,
         "verb_id": verb_id
     }
+
+
+def brute_force_label_space():
+    for label_id in range(256):
+        for label_sub_id in range(8):
+            for is_namespaced in range(2):
+                for is_single_object in range(2):
+                    for verb_id in range(8):
+                        try:
+                            label = encode_label(label_id, label_sub_id, is_namespaced, is_single_object, verb_id)
+                            decoded = decode_label(label)
+                            if decoded != "Unknown label":
+                                print(f"Label: {label}, {bin(label)}")
+                                print(f"Meaning: verb {decoded['verb']} on {decoded['apiGroup']}/{decoded['version']}/{decoded['uri']}; resource is {'namespaced' if decoded['is_namespaced'] else 'not namespaced'}; {'single object' if decoded['is_single_object'] else 'list of objects'}")
+                        except:
+                            continue
 
 
 def propose_label(j: dict):
@@ -146,23 +176,47 @@ def propose_label(j: dict):
 
 
 if __name__ == '__main__':
-    import sys
     from main import get_informative_string
 
-    file = sys.argv[1]
-    with open(file, 'r') as f:
-        for line in f:
-            j = json.loads(line)
-            label = propose_label(j)
+    parser = argparse.ArgumentParser(
+    prog='propose_label',
+    description='Manage automated labels')
+    parser.add_argument('--brute-force', action='store_true', help='Brute force the label space')
 
-            if label is None:
-                continue
-            try:
-                # print(bin(label))
-                print(f"{label} <- {get_informative_string(j)}")
-            except:
-                pass
+    parser.add_argument("-f", "--file", help="File to read from", type=str)
+    parser.add_argument("-e", "--encode", help="Encode a label", action='store_true')
 
-            # undecoded = decode_label(label)
-            # uri = undecoded["apiGroup"] + "/" + undecoded["version"] + "/" + undecoded["uri"]
-            # print(f"{label} -> {uri} {undecoded['verb']}")
+    parser.add_argument("-d", "--decode", help="Decode a label", action='store_true')
+    parser.add_argument("-l", "--label", help="Label to decode", type=int)
+
+    args = parser.parse_args()
+    if args.brute_force:
+        brute_force_label_space()
+        exit(0)
+    else:
+        if args.encode:
+            if not args.file:
+                print("Please provide a file to read from")
+                exit(1)
+
+            with open(args.file, 'r') as f:
+                for line in f:
+                    j = json.loads(line)
+                    label = propose_label(j)
+
+                    if label is None:
+                        continue
+                    try:
+                        # print(bin(label))
+                        print(f"{label} <- {get_informative_string(j)}")
+                    except:
+                        pass
+        elif args.decode:
+            if not args.label:
+                print("Please provide a label to decode")
+                exit(1)
+            decoded = decode_label(int(args.label))
+            print(f"{args.label} -> {decoded['apiGroup']}/{decoded['version']}/{decoded['uri']} {decoded['verb']}")
+            
+        else:
+            parser.print_help()
