827bba46bca799b0a68d09cb20ca2b8bc3d7303f
Matteo Franzil <mfranzil@fbk.eu>
2024-07-08 14:25:35 +0200

    fine tune splits

--- parseLog/parameters.py
+++ parseLog/parameters.py
@@ -8,8 +8,8 @@ INITIAL_LEARNING_RATE = float(os.getenv('INITIAL_LEARNING_RATE', 0.001))  # Init
 EARLY_STOPPING_PATIENCE = int(os.getenv('EARLY_STOPPING_PATIENCE', 40))  # Epochs before early stopping kicks in
 REDUCE_LR_FACTOR = float(os.getenv('REDUCE_LR_FACTOR', 0.5))  # Factor ReduceLRonPlateau reduces learning rate by
 REDUCE_LR_PATIENCE = int(os.getenv('REDUCE_LR_PATIENCE', 20))  # Epochs before ReduceLRonPlateau kicks in
-TEST_TRAIN_SPLIT = float(os.getenv('TEST_TRAIN_SPLIT', 0.1))  # Percentage of data to use for testing
-TRAIN_VALID_SPLIT = float(os.getenv('TRAIN_VALID_SPLIT', 0.2))  # Percentage of data to use for validation
+TEST_TRAIN_SPLIT = float(os.getenv('TEST_TRAIN_SPLIT', 0.25))  # Percentage of data to use for testing
+TRAIN_VALID_SPLIT = float(os.getenv('TRAIN_VALID_SPLIT', 0.25))  # Percentage of data to use for validation
 CONFUSION_MATRIX_TOP_PERCENTAGE = float(os.getenv('CONFUSION_MATRIX_TOP_PERCENTAGE', 0.2))
 # Top % of classes to show in confusion matrix
 LABEL_FEATURE = str(os.getenv('LABEL_FEATURE', 'label'))  # Name of the feature that contains the label
