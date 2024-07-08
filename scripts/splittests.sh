5891b103585d24df475cbcabaf23f34ae07dd3d1
Matteo Franzil <mfranzil@fbk.eu>
2024-07-08 19:35:03 +0200

    move and +x scripts

--- scripts/splittests.sh
+++ scripts/splittests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd ../parseLog
+cd ../parseLog || exit
 for train_test_split in $(seq 0.1 0.1 0.9); do
     for train_val_split in $(seq 0.1 0.1 0.9); do
         export TEST_TRAIN_SPLIT=$train_test_split
         export TRAIN_VALID_SPLIT=$train_val_split
-        python3 model.py -f ../logs/datasets/0-stitched-category-1a.json_cplabel_apionly ../logs/datasets/0-stitched-category-2.json
+        python3 model.py -f ../logs/datasets/0-stitched-category-1a.json_cplabel_apionly ../logs/datasets/0-stitched-category-2.json -s
     done
 done
