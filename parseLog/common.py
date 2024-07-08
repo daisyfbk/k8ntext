f9b6e5030e673528961357927565ebdb97a4084e
Matteo Franzil <mfranzil@fbk.eu>
2024-07-08 12:59:01 +0200

    change the way features are included for now

--- parseLog/common.py
+++ parseLog/common.py
@@ -1,3 +1,7 @@
+from typing import Iterable
+
+from tqdm import tqdm as tqdm_base
+
 IGNORED_NAMESPACES = {
     "falco",
     "kube-flannel"
@@ -6,9 +10,16 @@ LABEL_UNKNOWN = -1
 LABEL_IGNORE = -2
 
 
-from typing import Iterable
-from tqdm import tqdm as tqdm_base
-
 def tqdm(array: Iterable) -> Iterable:
     return tqdm_base(array, ncols=200, leave=False)
-    
+
+
+def exists_subkey(__object, *keys):
+    exists = True
+    for key in keys:
+        if key not in __object:
+            exists = False
+            break
+        __object = __object[key]
+
+    return exists
