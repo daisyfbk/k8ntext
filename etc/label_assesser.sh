4fcba9799d18ff2c437589ce6552e76cfe0544fd
Matteo Franzil <mfranzil@fbk.eu>
2024-07-10 11:27:49 +0200

    Spring cleaning

--- etc/label_assesser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find "$1" -name "*labelled" | while read -r file; do
-    echo "$file"
-    count=$(jq .label "$file" | grep -v "\-1" | uniq -c | sed 's/^\s*//')
-    label=$(echo "$count" | cut -f 2 -d " " | while read -r l; do python3 label_proposer.py -d -l "$l"; done)
-    echo "$label"
-    echo "----------"
-done
--- etc/label_proposer_to_table.sh
+++ etc/label_proposer_to_table.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+[[ -z $1 ]] && echo "Usage: $0 <file_name>" && exit 1
+FILE_NAME=$1
+
 echo 'label,username,verb,resource,namespace,name,requestURI,requestReceivedTimestamp' > tmp.csv
-python3 label_proposer.py -e -f $A | jq -rsc 'sort_by(.requestReceivedTimestamp) | .[] | [.label, .username, .verb, .resource, .namespace, .name, .requestURI, .requestReceivedTimestamp] | @csv' >> tmp.csv
+python3 label_proposer.py -e -f $FILE_NAME | jq -rsc 'sort_by(.requestReceivedTimestamp) | .[] | [.label, .username, .verb, .resource, .namespace, .name, .requestURI, .requestReceivedTimestamp] | @csv' >> tmp.csv
 pandoc tmp.csv -t org -o output.org
+
 cat <<EOF > output2.org
 #+OPTIONS: html-postamble:nil
 #+OPTIONS: html-preamble:nil
 EOF
+
 cat output2.org output.org > output3.org
 emacs --load ~/.emacs-export.el --batch --eval "(require 'org)" output3.org --funcall org-html-export-to-html
 sed -iE 's/.*#content.*//' output.html
 open output.html
+
 rm tmp.csv output.org output2.org output3.org
