6331528149f214045e6416853a700f8f1b68bcd2
Matteo Franzil <mfranzil@fbk.eu>
2024-07-13 18:26:09 +0200

    update dataset file paths in shell scripts

--- etc/windowtests.sh
+++ etc/windowtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../parseLog || exit
-for window_len in $(seq 5 5 200); do
+for window_len in $(seq 5 5 150); do
     export WINDOW_LENGTH=$window_len
-    python3 model.py -f ../logs/datasets/0-stitched-category-1a.json_cplabel_apionly ../logs/datasets/0-stitched-category-2.json -s
+    python3 model.py -f ../logs/datasets/0-stitched-category-1n.json_apionly ../logs/datasets/0-stitched-category-2n.json_apionly -s
 done
