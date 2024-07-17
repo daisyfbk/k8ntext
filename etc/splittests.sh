3748dea69f0b647f140e655a8e9496061c5bdbef
Matteo Franzil <mfranzil@fbk.eu>
2024-07-17 15:55:12 +0200

    chore: Update dataset file paths in shell scripts

--- etc/splittests.sh
+++ etc/splittests.sh
@@ -5,6 +5,6 @@ for train_test_split in $(seq 0.1 0.1 0.9); do
     for train_val_split in $(seq 0.1 0.1 0.9); do
         export TEST_TRAIN_SPLIT=$train_test_split
         export TRAIN_VALID_SPLIT=$train_val_split
-        python3 model.py -f ../logs/datasets/0-stitched-category-1n.json_apionly ../logs/datasets/0-stitched-category-2n.json_apionly -s
+        python3 model.py -f ../logs/datasets/0-stitched-category-*n.json_apionly -s
     done
 done
