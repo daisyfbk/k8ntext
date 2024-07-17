6e2f0390b635788b1669d13d5492853477fb071b
Matteo Franzil <mfranzil@fbk.eu>
2024-07-17 08:26:27 +0200

    fixed kube api

--- scripts/kube-api.sh
+++ scripts/kube-api.sh
@@ -12,10 +12,10 @@ path=$2
 shift
 shift
 
-kubectl config view --raw -o json | jq -r '.users[0].user."client-certificate-data"' | base64 -d > client.crt
-kubectl config view --raw -o json | jq -r '.users[0].user."client-key-data"' | base64 -d > client.key
-kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | base64 -d > ca.crt
+kubectl config view --raw --minify -o json | jq -r '.users[0].user."client-certificate-data"' | base64 -d > client.crt
+kubectl config view --raw --minify -o json | jq -r '.users[0].user."client-key-data"' | base64 -d > client.key
+kubectl config view --raw --minify -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | base64 -d > ca.crt
 
-curl -H "Content-Type: application/json" --key client.key --cert client.crt --cacert ca.crt -s  "$server/$path" "$@" | jq -c
-cd - || exit
+curl -H "Content-Type: application/json" --key client.key --cert client.crt --cacert ca.crt -s "$server/$path" "$@" -k 
+cd - &> /dev/null || exit
 rm -rf "$tmp"
