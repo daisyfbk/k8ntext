8a737c95175cec8ede88d7aeca2204a840139de0
Matteo Franzil <mfranzil@fbk.eu>
2024-07-17 15:55:12 +0200

    chore: Update dataset file paths in shell scripts

--- etc/windowtests.sh
+++ etc/windowtests.sh
@@ -3,5 +3,5 @@
 cd ../parseLog || exit
 for window_len in $(seq 5 5 150); do
     export WINDOW_LENGTH=$window_len
-    python3 model.py -f ../logs/datasets/0-stitched-category-1n.json_apionly ../logs/datasets/0-stitched-category-2n.json_apionly -s
+    python3 model.py -f ../logs/datasets/0-stitched-category-*n.json_apionly -s
 done
