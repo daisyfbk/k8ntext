f55d49cfbb6b35a9e07ed14ae71a0160851a2ed7
Matteo Franzil <mfranzil@fbk.eu>
2024-07-19 17:06:30 +0200

    remove redudant parameter and change splits again

--- parseLog/parameters.py
+++ parseLog/parameters.py
@@ -8,9 +8,8 @@ INITIAL_LEARNING_RATE = float(os.getenv('INITIAL_LEARNING_RATE', 0.004))  # Init
 EARLY_STOPPING_PATIENCE = int(os.getenv('EARLY_STOPPING_PATIENCE', 8))  # Epochs before early stopping kicks in
 REDUCE_LR_FACTOR = float(os.getenv('REDUCE_LR_FACTOR', 0.1))  # Factor ReduceLRonPlateau reduces learning rate by
 REDUCE_LR_PATIENCE = int(os.getenv('REDUCE_LR_PATIENCE', 4))  # Epochs before ReduceLRonPlateau kicks in
-TEST_TRAIN_SPLIT = float(os.getenv('TEST_TRAIN_SPLIT', 0.25))  # Percentage of data to use for testing
-TRAIN_VALID_SPLIT = float(os.getenv('TRAIN_VALID_SPLIT', 0.25))  # Percentage of data to use for validation
-CONFUSION_MATRIX_TOP_PERCENTAGE = float(os.getenv('CONFUSION_MATRIX_TOP_PERCENTAGE', 0.2))
+TEST_TRAIN_SPLIT = float(os.getenv('TEST_TRAIN_SPLIT', 0.1))  # Percentage of data to use for testing
+TRAIN_VALID_SPLIT = float(os.getenv('TRAIN_VALID_SPLIT', 0.1))  # Percentage of data to use for validation
 TUNER_EXECUTIONS_PER_TRIAL = int(os.getenv('TUNER_EXECUTIONS_PER_TRIAL', 5))  
 # Top % of classes to show in confusion matrix
 LABEL_FEATURE = str(os.getenv('LABEL_FEATURE', 'label'))  # Name of the feature that contains the label
