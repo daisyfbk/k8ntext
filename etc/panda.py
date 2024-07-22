33eb0ce43125e3b691daa9d217383338b4ba78af
Matteo Franzil <mfranzil@fbk.eu>
2024-07-22 10:36:24 +0200

    two scrub scripts

---
+++ etc/panda.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
+
+data = json.loads(open("../logs/models/data.json").read())
+
+# Convert the JSON data to a pandas DataFrame
+df = pd.DataFrame(data).sort_values(by='precision')
+
+# Plotting
+fig, ax = plt.subplots()
+# Assuming 'mode' is categorical and you want different colors for each mode
+colors = {'tensor': 'blue', 'number': 'red'}
+for mode, color in colors.items():
+    mode_df = df[df['mode'] == mode]
+    ax.scatter(mode_df['precision'], mode_df['error_statistics'], c=color, label=mode)
+
+# Adding labels and title for clarity
+ax.set_xlabel('Precision')
+ax.set_ylabel('Number of Correct Predictions')
+ax.set_title('Scatter Plot of Precision vs Actual Number of Correct Predictions')
+plt.legend()
+plt.show()
