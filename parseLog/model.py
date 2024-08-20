1c1a76b561f5bc242b73eeaaa2c48d7ef8a5d4d6
Matteo Franzil <mfranzil@fbk.eu>
2024-08-20 10:33:19 +0200

    scaled back model

--- parseLog/model.py
+++ parseLog/model.py
@@ -129,9 +129,9 @@ def preprocess_data(__data: list[dict],
 def generate_model(X_shape: int, y_shape: int | tuple) -> models.Model:
     model = models.Sequential([
         layers.Input(shape=(pm.WINDOW_LENGTH, X_shape)),
-        layers.Bidirectional(layers.LSTM(X_shape * 8, return_sequences=True, name='lstm_1'), name='bidirectional_1'),
+        layers.Bidirectional(layers.LSTM(X_shape * 4, return_sequences=True, name='lstm_1'), name='bidirectional_1'),
         layers.BatchNormalization(name='batch_norm_1'),
-        layers.Bidirectional(layers.LSTM(X_shape * 8, return_sequences=True, name='lstm_2'), name='bidirectional_2'),
+        layers.Bidirectional(layers.LSTM(X_shape * 3, return_sequences=True, name='lstm_2'), name='bidirectional_2'),
         layers.Dropout(0.4, name='dropout_1'),
         layers.TimeDistributed(layers.Dense(y_shape[0] * y_shape[1], activation='relu', name='dense'),
                                name='time_distributed'),
