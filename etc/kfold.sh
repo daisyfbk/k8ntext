5ecd1a36585c09709ea5a292b013caee14e885fa
Matteo Franzil <mfranzil@fbk.eu>
2024-09-20 23:42:42 +0200

    kfolds script

---
+++ etc/kfold.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ../parseLog || exit
+for split in $(seq 1 1 20); do
+    export STATISTICS_ATTEMPTS=$split
+    python3 model.py -f ../logs/datasets/0-stitched-category-*n.json_apionly -s kfolds,save
+done
