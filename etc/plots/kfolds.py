5ac4e0c26e1e8521142f05f740058a03df7e0bfb
Matteo Franzil <mfranzil@fbk.eu>
2024-09-22 23:03:20 +0200

    more!!!

---
+++ etc/plots/kfolds.py
@@ -0,0 +1,101 @@
+# #
+# INFO - STATISTICS_ATTEMPTS: 19
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 15:48:04,223 - INFO - Accuracy on labeled: 0.9396267837541163 (errors: 55 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 15:53:35,112 - INFO - Accuracy on labeled: 0.9769484083424808 (errors: 21 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 15:59:36,471 - INFO - Accuracy on labeled: 0.9385290889132821 (errors: 56 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:06:24,028 - INFO - Accuracy on labeled: 0.9484083424807903 (errors: 47 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:11:59,614 - INFO - Accuracy on labeled: 0.9824368825466521 (errors: 16 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:18:09,207 - INFO - Accuracy on labeled: 0.9549945115257958 (errors: 41 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:24:15,107 - INFO - Accuracy on labeled: 0.9187705817782656 (errors: 74 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:31:08,020 - INFO - Accuracy on labeled: 0.7694840834248079 (errors: 210 / 911)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:36:41,739 - INFO - Accuracy on labeled: 0.9021978021978022 (errors: 89 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:42:26,882 - INFO - Accuracy on labeled: 0.9461538461538461 (errors: 49 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:49:07,231 - INFO - Accuracy on labeled: 0.9681318681318681 (errors: 29 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:54:11,067 - INFO - Accuracy on labeled: 0.9637362637362638 (errors: 33 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 16:59:38,849 - INFO - Accuracy on labeled: 0.9461538461538461 (errors: 49 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 17:06:27,733 - INFO - Accuracy on labeled: 0.8681318681318682 (errors: 120 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 17:12:15,413 - INFO - Accuracy on labeled: 0.8681318681318682 (errors: 120 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 17:20:32,978 - INFO - Accuracy on labeled: 0.8945054945054945 (errors: 96 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 17:27:21,077 - INFO - Accuracy on labeled: 0.8054945054945055 (errors: 177 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 17:33:22,528 - INFO - Accuracy on labeled: 0.8065934065934066 (errors: 176 / 910)
+#[20240921_154145.840196__mfranzil-gpu] - 2024-09-21 17:40:28,513 - INFO - Accuracy on labeled: 0.5637362637362637 (errors: 397 / 910)
+#
+# INFO - STATISTICS_ATTEMPTS: 20
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 17:48:39,178 - INFO - Accuracy on labeled: 0.9477958236658933 (errors: 45 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 17:55:30,514 - INFO - Accuracy on labeled: 0.9721577726218097 (errors: 24 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:02:57,293 - INFO - Accuracy on labeled: 0.951276102088167 (errors: 42 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:10:17,626 - INFO - Accuracy on labeled: 0.95707656612529 (errors: 37 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:20:02,197 - INFO - Accuracy on labeled: 0.9733178654292344 (errors: 23 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:26:25,108 - INFO - Accuracy on labeled: 0.9559164733178654 (errors: 38 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:33:11,046 - INFO - Accuracy on labeled: 0.931554524361949 (errors: 59 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:39:09,216 - INFO - Accuracy on labeled: 0.8689095127610209 (errors: 113 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:45:31,052 - INFO - Accuracy on labeled: 0.8120649651972158 (errors: 162 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:51:55,899 - INFO - Accuracy on labeled: 0.9303944315545244 (errors: 60 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 18:59:20,960 - INFO - Accuracy on labeled: 0.9593967517401392 (errors: 35 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:04:55,439 - INFO - Accuracy on labeled: 0.9779582366589327 (errors: 19 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:13:24,594 - INFO - Accuracy on labeled: 0.9489559164733179 (errors: 44 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:19:47,810 - INFO - Accuracy on labeled: 0.925754060324826 (errors: 64 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:26:31,990 - INFO - Accuracy on labeled: 0.8410672853828306 (errors: 137 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:32:36,974 - INFO - Accuracy on labeled: 0.894431554524362 (errors: 91 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:38:50,766 - INFO - Accuracy on labeled: 0.888631090487239 (errors: 96 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:46:09,185 - INFO - Accuracy on labeled: 0.7819025522041764 (errors: 188 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:52:10,004 - INFO - Accuracy on labeled: 0.7935034802784223 (errors: 178 / 862)
+#[20240921_174056.520092__mfranzil-gpu] - 2024-09-21 19:58:07,552 - INFO - Accuracy on labeled: 0.6968641114982579 (errors: 261 / 861)
+#
+#
+#find . -type d -name "*gpu" | sort | while read folder; do cat $folder/main.log | cut -f 6- -d "-" | grep STATISTICS | uniq; grep 'Accuracy on labeled' $folder/main.log; echo; done
+
+import json
+
+lines = []
+with open('/Users/matte/Library/CloudStorage/OneDrive-FondazioneBrunoKessler/projects/audit/results/1_paper_tests/3_kfold_tests/metrics', 'r') as f:
+    lines = f.readlines()
+
+runs = []
+collected_features = set()
+
+r = {}
+for i in range(len(lines)):
+    line = lines[i]
+    if line == "" or line == "\n" or line == " ":
+        assert len(r['trials']) == r['attempts']
+        runs.append(r)
+        r = {}
+        continue
+    # first expected line STATISTICS_ATTEMPTS
+    if 'STATISTICS_ATTEMPTS' in line:
+        r['attempts'] = int(line.split(': ')[1])
+        r['trials'] = []
+    if 'Accuracy on labeled' in line:
+        r['trials'].append(float(line.split(': ')[1].split(' ')[0]))
+
+#print(runs)
+#assert len(r['trials']) == r['attempts'], f"Expected {r['attempts']} trials, got {len(r['trials'])}"
+#runs.append(r)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+runs = [r for r in runs if 4 < len(r['trials']) <= 10]
+
+# colourblind-friendly colours
+cmap = plt.get_cmap('tab10')
+markers = ['o', 's',  'v', '>', 'P', '*', 'X']
+
+plt.figure(figsize=(7, 4), dpi=300)
+# print all runs rescaled to the same length
+for i in range(len(runs)):
+    r = runs[i]
+    x = np.linspace(0, 14818, len(r['trials']))
+    y = r['trials']
+    plt.plot(x, y, marker=markers[i], color=cmap(i), linestyle='-', linewidth=1, markersize=5)
+
+# put a 'provisional' watermark
+plt.text(0.5, 0.5, 'Provisional plot', horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes, fontsize=20, alpha=0.5)
+
+plt.xlabel('Dataset split used for training')
+plt.ylabel('Accuracy (correct/total)')
+plt.title('K-fold cross validation accuracy')
+plt.legend([f"K={r['attempts']}" for r in runs])
+plt.tight_layout()
+plt.savefig('kfolds.png')
